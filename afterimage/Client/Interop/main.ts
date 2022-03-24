80c250f39df9bf24ff4ce6513db42e85d1dad2fc
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2022-03-25 07:21:27 +1300

    Add working image upload display

--- afterimage/Client/Interop/main.ts
+++ afterimage/Client/Interop/main.ts
@@ -1,13 +1,19 @@
-﻿class Interop {
-    public hello() {
-        return alert('Hello');
-    }
-    public showPrompt(message: string): string {
-        return prompt(message, 'Type anything here');
-    }
+﻿// Stream returned by .NET
+// unsure as to full contract
+interface IStream {
+  arrayBuffer: () => ArrayBuffer;
+}
+
+async function getImageUrl(imageStream: IStream) {
+  const buffer = await imageStream.arrayBuffer();
+  const blob = new Blob([buffer]);
+  const url = URL.createObjectURL(blob);
+  return url;
 }
 
 function Load() {
-    window['interop'] = new Interop();
+  window["interop"] = {
+     getImageUrl,
+  };
 }
 Load();
